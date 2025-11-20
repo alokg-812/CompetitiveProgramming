8ed2506e3f4a06f9d84653919279e31d02e6808a
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-11-20 16:21:35 +0530

    Add solution for EXUNPIZZA problem
    Implement solution for the EXUNPIZZA problem on CodeChef.

---
+++ contests/codechef/Starter_213/aExunAndThePizzas.cpp
@@ -0,0 +1,11 @@
+// https://www.codechef.com/problems/EXUNPIZZA
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int k,n,r;
+	cin>>n>>k>>r;
+	cout<<((n*r)-(k*r))<<endl;
+
+}
