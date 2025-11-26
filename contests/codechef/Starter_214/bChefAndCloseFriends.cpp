a5acd6c04fd9daacc4ba900db5f27c57a5692796
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-11-26 23:23:19 +0530

    Add bChefAndCloseFriends.cpp solution

---
+++ contests/codechef/Starter_214/bChefAndCloseFriends.cpp
@@ -0,0 +1,18 @@
+// https://www.codechef.com/problems/P4HOME
+// https://www.codechef.com/viewsolution/1211903021
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    cout<<2*min(y,z)<<endl;
+	    
+	}
+
+}
