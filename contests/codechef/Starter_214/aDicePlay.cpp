dfba98ec6f51a093ab49b2cb264292b3a513bc6f
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-11-26 23:22:32 +0530

    Add aDicePlay.cpp for dice game solution
    Implement a simple dice game that checks if two inputs are equal.

---
+++ contests/codechef/Starter_214/aDicePlay.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/problems/P1HOME
+// https://www.codechef.com/viewsolution/1211886028
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a>>b;
+	if(a==b){
+	    cout<<"Yes\n";
+	}
+	else cout<<"No\n";
+
+}
