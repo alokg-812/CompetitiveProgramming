8f5dc74dd37795574af22ffcd5c981d4c4506a90
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-12-03 23:31:19 +0530

    Add solution for CodeChef COMP4 problem
    Implement solution for CodeChef problem COMP4.

---
+++ contests/codechef/Starter_215/aCOMP4.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/COMP4
+// https://www.codechef.com/viewsolution/1214298930
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	int pw = pow(2, 4-x);
+	cout<<1000*pw<<endl;
+
+}
