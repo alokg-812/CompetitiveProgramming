040f2680a01416718a56a1a0ed9bb2e18825eef0
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-12-10 23:20:20 +0530

    Create aEntertainments.cpp

---
+++ contests/codechef/Starter_216/aEntertainments.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/START216D/problems/ENTERTAIN
+// https://www.codechef.com/viewsolution/1217057118
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	cout<<min((1000),(200*n))<<endl;
+
+}
