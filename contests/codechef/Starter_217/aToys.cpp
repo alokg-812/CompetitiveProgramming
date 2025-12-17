86b34ce0bb54b452ac1934439fbe27a18dd3a4b4
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-12-17 22:08:58 +0530

    Add aToys.cpp solution for CodeChef Starter 217

---
+++ contests/codechef/Starter_217/aToys.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/START217D/problems/TOYS
+// https://www.codechef.com/viewsolution/1219168354
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,m;
+	cin>>n>>m;
+	if(n>=m){
+	    cout<<n-m<<endl;
+	}
+	else{
+	    cout<<0<<endl;
+	}
+
+}
