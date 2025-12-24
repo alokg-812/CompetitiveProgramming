42101a60bac9fe8cc6de2e8955552a3559cf1211
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-12-24 22:18:44 +0530

    Add aChristmasTree.cpp for CodeChef problem

---
+++ contests/codechef/Starter_218/aChristmasTree.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/START218D/problems/XMASTREE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int m,n,a,b;
+	cin>>a>>m>>b>>n;
+	cout<<((n*b)-(a*m))<<endl;
+	
+
+}
