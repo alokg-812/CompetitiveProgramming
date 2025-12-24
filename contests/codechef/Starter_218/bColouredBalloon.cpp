1ca74b9eca08494b86d4dfb7730fee9147f5dd7c
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-12-24 22:19:14 +0530

    Add bColouredBalloon.cpp for problem solution
    Implement solution for CodeChef problem BALLOONS.

---
+++ contests/codechef/Starter_218/bColouredBalloon.cpp
@@ -0,0 +1,23 @@
+// https://www.codechef.com/START218D/problems/BALLOONS
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    vector<int>arr(n);
+	    int sum = 0;
+	    for(int i = 0;i<n;i++){
+	        cin>>arr[i];
+	        sum += ((i+1)*arr[i]);
+	    }
+	    cout<<sum<<endl;
+	}
+
+}
