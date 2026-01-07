11f6045d6dda9740573d50c24fc393e7db314887
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2026-01-07 23:34:57 +0530

    Add cTwoPileGame.cpp for two-pile game logic

---
+++ contests/codechef/Starter_220/cTwoPileGame.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void check(){
+    long long x,y;
+    cin>>x>>y;
+    if(x%2!=0){
+        cout<<"Alice\n";
+    }
+    else{
+        cout<<"Bob\n";
+    }
+    return;
+}
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        check();
+    }
+}
