d1b3935b3634b2dc9f8d79e3ab3fee0fb0100f26
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2026-02-25 21:49:26 +0530

    Add BBuyingTickets.cpp solution for CodeChef problem

---
+++ contests/codechef/Starter_227/BBuyingTickets.cpp
@@ -0,0 +1,44 @@
+// https://www.codechef.com/START227C/problems/BUYTICK
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void check(){
+    int n,k;
+    cin>>n>>k;
+    vector<int>arr(n);
+    for(int i = 0;i<n;i++){
+        cin>>arr[i];
+    }
+    string str;
+    cin>>str;
+    vector<int> avail;
+    for(int i = 0;i<n;i++){
+        if(str[i] == '0'){
+            avail.push_back(arr[i]);
+        }
+    }
+    if((int)avail.size() <k){
+        cout<<-1 <<'\n';
+        return;
+    }
+    sort(avail.begin(), avail.end());
+    int mini = 0;
+    for(int i = 0;i<k;i++){
+        mini += avail[i];
+    }
+    cout<<mini<<'\n';
+    return;
+}
+
+
+int main() {
+	// your code goes here
+	int t;
+    cin>>t;
+    while(t--){
+        check();
+    }
+}
+
