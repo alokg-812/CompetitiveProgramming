c794e9632500957f4b93a8a64374bb65c7235b1d
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2026-02-25 21:48:36 +0530

    Add ABankGlitch.cpp solution for CodeChef problem

---
+++ contests/codechef/Starter_227/ABankGlitch.cpp
@@ -0,0 +1,28 @@
+// https://www.codechef.com/START227C/problems/BANKGLITCH
+
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void check(){
+    int a,b,x,y;
+    cin>>a>>b>>x>>y;
+    int ans = a+b;
+    int maxi = a/x;
+    for(int k = 0; k<=maxi;k++){
+        int tot = (a - k*x) + (b + k*y);
+        if(tot>ans) ans = tot;
+    }
+    cout<<ans<<'\n';
+    return;
+}
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        check();
+    }
+}
